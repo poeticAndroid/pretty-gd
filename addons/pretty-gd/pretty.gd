f2a899556526545074876bf10c9df6830673a72f
poeticAndroid <i@poeticandroid.online>
2026-02-17 10:44:17 +0100

    basic framework

--- addons/pretty-gd/pretty.gd
+++ addons/pretty-gd/pretty.gd
@@ -1,38 +1,65 @@
 class_name Prettifier
 
+static var indent="\t"
+
 static var input
 static var pos
 
 static func prettify(_input:String)->String:
 	input=_input
 	pos=0
+	var output=""
 	while not is_eof(): 
-		print(read_token() )	
-	return ""
+		output+=read_line() 	
+	return output
 	
 		
 static func read_line():
-	pass
+	var line=read_whitespace()
+	while not is_eol ( ) :
+		line +=  read_tøken()+" "
+	line= line.strip_edges(false,true)+ read()
+	return line
 		
 
-static func read_token():
+static func read_tøken() :
 	var token=""
-	while not ( is_eof() or peek().strip_edges() ):
-		read()
-	while not is_eof() and peek().strip_edges() :
+	read_whitespace ( )
+	token =  read_word ( )
+	return  token
+	
+	
+static func read_word():
+	var token=""
+	while  peek().strip_edges() :
+		token+=read( )
+	return token
+	
+static func read_whitespace():
+	var token=""
+	while not is_eol() and not peek().strip_edges() :
 		token+=read( )
 	return token
 	
 	
-static func read():
+static func read(len=1):
 	if is_eof():return ""
-	var char= input[pos]
-	pos+=1
-	return char
+	pos+=len
+	return input.substr(pos-len,len)
 
-static func peek():
+static func peek(len=1):
 	if is_eof():return ""
-	return input[pos]
+	return input.substr(pos,len)
+
+static func is_eol():
+	return is_eof() or  peek()=="\n"
 
 static func is_eof():
 	return pos>=input.length()
+
+const keywords = ["if", "else", "elif", "for", "while", "break", "continue",
+  "pass", "return", "class", "class_name", "extends", "is", "as", "signal",
+  "static", "const", "enum", "var", "breakpoint", "yield", "in", "and", "or"]
+const longsymbols = ["**", "<<", ">>", "==", "!=", ">=", "<=", "&&", "||",
+  "+=", "-=", "*=", "/=", "%=", "**=", "&=", "^=", "|=", "<<=", ">>=",
+  ":=", "->"]
--- main.gd
+++ main.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello")
-	Prettifier.prettify( FileAccess.get_file_as_string("res://main.gd") )
+	print(Prettifier.prettify( FileAccess.get_file_as_string("res://addons/pretty-gd/pretty.gd") ) )
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
