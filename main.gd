fd4e3c292b5e3549df1aa5d2cc817c12fc592ebd
poeticAndroid <i@poeticandroid.online>
2026-03-02 20:14:24 +0100

    proper export attributes

--- main.gd
+++ main.gd
@@ -5,6 +5,13 @@ var Prettifier = preload("res://addons/pretty-gd/pretty.gd").new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var file = FileAccess.open("res://addons/pretty-gd/README.md", FileAccess.WRITE)
+	file.store_string(FileAccess.get_file_as_string("res://README.md"))
+	file.close()
+	file = FileAccess.open("res://addons/pretty-gd/LICENSE", FileAccess.WRITE)
+	file.store_string(FileAccess.get_file_as_string("res://LICENSE"))
+	file.close()
+
 	print(Prettifier.prettify(FileAccess.get_file_as_string("res://addons/pretty-gd/pretty.gd")))
 	var ascii = ""
 	for i in range(32, 127):
